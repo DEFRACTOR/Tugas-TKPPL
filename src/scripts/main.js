cee63c22403c146508fb568aebce04d7e9e11c03
Ghendra <happy.hendratandiono@gmail.com>
2016-05-30 14:45:21 +0700

    First empty jade website

---
+++ src/scripts/main.js
@@ -0,0 +1,12 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke'),
+  classes = require('bespoke-classes'),
+  keys = require('bespoke-keys'),
+  touch = require('bespoke-touch');
+
+// Bespoke.js
+bespoke.from('article', [
+  classes(),
+  keys(),
+  touch()
+]);
